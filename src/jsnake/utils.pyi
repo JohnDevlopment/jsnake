f7d893d120f6bd3db7c83ce1e867c82498350ec0
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2023-10-09 16:28:28 -0500

    Annotate utils.binary_search parameters in type stub

--- src/jsnake/utils.pyi
+++ src/jsnake/utils.pyi
@@ -29,7 +29,7 @@ class attr_dict(dict[str, Any]):
     def __setattr__(self, key: str, value) -> None:
         ...
 
-def binary_search(array, pattern) -> int:
+def binary_search(array: Any, pattern: Any) -> int:
     ...
 
 class readonly_dict(dict[str, Any]):

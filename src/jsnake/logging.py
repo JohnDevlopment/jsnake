f04de2f4e76db02d095a67cabc682449d4c11392
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2023-10-16 13:57:27 -0500

    Refactor logging.py for Sphinx
    Also, remove `unittest' code

--- src/jsnake/logging.py
+++ src/jsnake/logging.py
@@ -1,4 +1,23 @@
-"""Logging module."""
+"""
+Logging module.
+
+.. note:: Before you can start using the other
+   methods, call :py:func:`init`.
+
+This module includes functions for creating and modifying loggers.
+When you first initialize this module via :py:func:`init`, ``jsnake``
+sets the default severity level via the environment. By default,
+:py:func:`init` looks for ``JSNAKE_LEVEL`` in the environment, but you
+can set the prefix "JSNAKE" to something else (see function for details).
+
+.. code-block:: python
+
+   from jsnake.logging import init, get_logger
+   logger = get_logger('foo')
+   logger.info("Info")
+   logger.info("My name is %s", "Foo")
+"""
+
 from __future__ import annotations
 from .utils import get_env
 from enum import IntEnum
@@ -30,6 +49,12 @@ class Level(IntEnum):
         return cls.__members__.get(key)
 
 def _get_default_level(envname: str) -> Level:
+    """
+    Set the default logging level using ENVNAME.
+
+    If ENVNAME is undefined or its value is invalid,
+    `Level.INFO` is returned.
+    """
     level = get_env(envname)
     if level is not None:
         try:
@@ -53,9 +78,9 @@ _rootLogger: Logger
 _cache: dict[str, Logger] = {}
 _initialized = False
 
-def init(name: str, envprefix: str="JSNAKE"):
+def init(name, envprefix="JSNAKE"):
     """
-    Initialize the logging hiererchy.
+    Initialize the logging module.
 
     :param str name: The name of the root logger
 
@@ -63,10 +88,12 @@ def init(name: str, envprefix: str="JSNAKE"):
                           to get the default logging level
 
     The default level for loggers is taken from the environment variable
-    ``X_LEVEL``, where ``X`` is the value of `envprefix`.
+    ``X_LEVEL``, where ``X`` is the value of `envprefix`. For example,
+    if using the default of "JSNAKE", the environment name used will
+    be ``JSNAKE_LEVEL``.
 
-    .. warning::
-       This MUST be called before any other function in this module.
+    .. note::
+       This **must** be called before any other function in this module.
     """
     global _rootLogger, _initialized, DEFAULT_LEVEL
 
@@ -93,21 +120,26 @@ def add_handler(logger: Logger, kind: str, **kw):
     :raises LoggingError: If the logging module has not been initialized, or
                           if `kind` is invalid
 
-    .. note:: Keyword arguments
+    Keyword arguments
+    -----------------
 
-       The keyword arguments depend on what `kind` of handler to add.
+    The keyword arguments depend on what `kind` of handler to add.
 
-       stream:
-         * stream = the stream to output to. Should be derived from IO.
-                    If omitted, defaults to ``sys.stderr``
+    * stream
+        * stream
+            * The stream to output to. Should be derived from IO.
+              If omitted, defaults to ``sys.stderr``.
+    * file
+        * mode
+            * Same as the mode argument in open() (default: 'wt').
 
-       file:
-         * mode = same as the mode argument in open() (default: 'wt')
-         * encoding = same as the encoding argumet in open()
-         * file = the name of the file
+        * encoding
+            * Same as the encoding argumet in open().
 
-       null:
-         none
+        * file
+            * The name of the file.
+    * null
+        * No arguments.
     """
     if not _initialized:
         raise LoggingError("You must call init() before using any of the other functions.")
@@ -130,9 +162,9 @@ def add_handler(logger: Logger, kind: str, **kw):
 
     logger.addHandler(hdl)
 
-def get_logger(name: str="", level: Level | None=None, stream: bool=True):
+def get_logger(name="", level=None, stream=True):
     """
-    Returns a logger with the specified name.
+    Return a logger with the specified `name`.
 
     :param str name: The name of the logger
 
@@ -146,13 +178,30 @@ def get_logger(name: str="", level: Level | None=None, stream: bool=True):
              in which case the root logger
     :rtype: Logger
 
-    .. note::
+    :raises LoggingError: If :py:func:`init` was not called
+                          prior to calling this function
 
-       Any logger returned by this is part of the
-       logger hierchy. That is to say, a logger named
-       `io` is a direct child of the root logger. And
-       `io.read` is a direct descendent of `io`, which
-       is a descendent of the root logger.
+    A new logger is created upon the first invocation with
+    the specified `name`. If `stream` equals true, a stream
+    handler is added to it, but this only applies to the
+    creation of the logger. The newly created logger is cached
+    and then returned on subsequent calls with the same `name`.
+
+    To add a stream handler to the logger after its creation,
+    call :py:func:`add_handler`.
+
+    Loggers created through this function are actually from
+    Python's ``logging`` module. This matters because of
+    a certain trait that loggers have.
+
+    .. note:: Logging Hierarchy
+       A logger's `name` can potentially be a dot-separated
+       hierarchical value, like ``foo.bar.baz`` (though it could
+       also be just plain ``foo``). Loggers that are further down
+       the hierarchical list are children of the loggers which
+       are higher up the list. For example, given the `name`
+       ``foo``, loggers ``foo.bar``, ``foo.bar.baz``, and
+       ``foo.bam``, are all descendants of ``foo``.
     """
     global _cache
 
@@ -162,7 +211,8 @@ def get_logger(name: str="", level: Level | None=None, stream: bool=True):
     # Return the root logger if name is "" or "yt_dlp_tk"
     parts = name.split(".")
     isroot = parts[0] == "" and len(parts) == 1
-    if isroot: return _rootLogger
+    if isroot:
+        return _rootLogger
 
     # Insert root name into the array
     root_name = _rootLogger.name
@@ -194,28 +244,3 @@ def get_logger(name: str="", level: Level | None=None, stream: bool=True):
     _cache[name] = logger
 
     return logger
-
-if __debug__:
-    import unittest, tempfile
-    from pathlib import Path
-
-    class TestLogging(unittest.TestCase):
-        def test_logger(self):
-            init("logging")
-
-            logger = get_logger('test1', Level.INFO)
-            with self.assertLogs(logger, Level.INFO) as cm:
-                logger.info("first message")
-
-            logger = get_logger('test1.bar', stream=False)
-            with self.assertLogs(logger, Level.INFO) as cm:
-                logger.info("second message")
-
-            with tempfile.TemporaryDirectory() as tmpdir:
-                file_path = Path(tmpdir) / "asfsdf.log"
-                add_handler(logger, 'file', file=str(file_path))
-                with self.assertLogs(logger, Level.INFO) as cm:
-                    logger.info("third message")
-
-    if __name__ == "__main__":
-        unittest.main()

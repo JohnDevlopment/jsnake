bc77a1d345e1b10064c1e10dc9dba119663f8c85
JohnDevlopment <50782633+JohnDevlopment@users.noreply.github.com>
2023-11-15 08:55:44 -0600

    Rename ExEntry 'text' option to 'label'

--- src/jsnake/interface/entry.py
+++ src/jsnake/interface/entry.py
@@ -13,7 +13,7 @@ class ExEntry(ttk.Entry, _WidgetMixin):
     """Extended entry widget."""
 
     def __init__(self, master: _Widget=None, *, scrollx: bool=False,
-                 text: str | None=None, clearbutton: bool=False, **kw: Any):
+                 label: str | None=None, clearbutton: bool=False, **kw: Any):
         """
         Construct an extended entry widget with the parent `master`.
 
@@ -24,8 +24,8 @@ class ExEntry(ttk.Entry, _WidgetMixin):
 
         :keyword bool scrollx: If true, a horizontal scrollbar is added
 
-        :keyword text: A textual string to display as a label next to the entry
-        :type text: str or None
+        :keyword label: A textual string to display as a label next to the entry
+        :type label: str or None
         """
         # Add signal 'x_scrollbar_changed'
         self.on_x_scrollbar_changed = signal('x_scrollbar_changed', self)
@@ -46,14 +46,14 @@ class ExEntry(ttk.Entry, _WidgetMixin):
         # Frame this enter goes inside
         self.frame = ttk.Frame(master, padding='0 0 0 16')
 
-        # Label that displays the contents of the text property
-        self.label = ttk.Label(self.frame, text=text or "")
+        # Label that displays the contents of the label property
+        self.label = ttk.Label(self.frame, text=label or "")
 
         # X scrollbar
         self.xbar = ttk.Scrollbar(self.frame, orient=tk.HORIZONTAL, command=self.xview)
 
         # Button that clears the text
-        self.clearbutton = ttk.Button(self.frame, text="X", command=self.clear)
+        self.clearbutton = ttk.Button(self.frame, text="X", command=self.clear, width=2)
 
         # Bind the X scroll command to the scrollbar
         kw['xscrollcommand'] = self.xbar.set
@@ -68,14 +68,14 @@ class ExEntry(ttk.Entry, _WidgetMixin):
         self.entry_name = ttk.Entry.__str__(self)
 
         # Custom resources
-        self.set_custom_resources(scrollx=scrollx, text=text, clearbutton=clearbutton)
+        self.set_custom_resources(scrollx=scrollx, label=label, clearbutton=clearbutton)
 
         # Pack the entry
         self.grid(row=0, column=1)
 
         # Emit signals so that the program can react to the initialized options
         self.on_x_scrollbar_changed.emit(scrollx)
-        self.on_label_changed.emit(text or "")
+        self.on_label_changed.emit(label or "")
         self.on_clearbutton_changed.emit(clearbutton)
         self.on_text_changed.emit("")
 
@@ -88,7 +88,7 @@ class ExEntry(ttk.Entry, _WidgetMixin):
 
     def configure(self, *, scrollx: bool | None=None,
                   clearbutton: bool | None=None,
-                  text: str | None=None, **kw: Any):
+                  label: str | None=None, **kw: Any):
         if scrollx is not None:
             self.set_custom_resource('scrollx', scrollx)
             self.on_x_scrollbar_changed.emit(scrollx)
@@ -97,9 +97,9 @@ class ExEntry(ttk.Entry, _WidgetMixin):
             self.set_custom_resource('clearbutton', clearbutton)
             self.on_clearbutton_changed.emit(clearbutton)
 
-        if text is not None:
-            self.set_custom_resource('text', text)
-            self.on_label_changed.emit(text)
+        if label is not None:
+            self.set_custom_resource('label', label)
+            self.on_label_changed.emit(label)
 
         super().configure(kw)
 
@@ -110,8 +110,8 @@ class ExEntry(ttk.Entry, _WidgetMixin):
         return super().cget(key)
 
     def config(self, *, scrollx: bool | None=None,
-                  text: str | None=None, **kw: Any):
-        return self.configure(scrollx=scrollx, text=text, **kw)
+                  label: str | None=None, **kw: Any):
+        return self.configure(scrollx=scrollx, label=label, **kw)
 
     def __validate_entry(self,
                          reason: Literal['focusin', 'focusout', 'key', 'forced'],
@@ -138,7 +138,7 @@ class ExEntry(ttk.Entry, _WidgetMixin):
                 # Map the label
                 self.label.grid(row=0, column=0)
 
-            # Configure the text
+            # Configure the label
             label.configure(text=new_text)
 
             return
